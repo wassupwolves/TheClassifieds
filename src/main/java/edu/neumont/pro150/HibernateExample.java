2c79939dd25f2ecc943a55d2ee8d28a6e1f5652d
MShultz <mshultz@student.neumont.edu>
2016-11-09 17:44:07 -0700

    Initial Commit

--- src/main/java/edu/neumont/pro150/HibernateExample.java
+++ src/main/java/edu/neumont/pro150/HibernateExample.java
@@ -5,13 +5,13 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceUnit;
 
+import org.hibernate.SessionFactory;
+
 public class HibernateExample {
 	
 	@PersistenceUnit
 	protected void setUp() throws Exception {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("edu.neumont.pro150" );
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		
+		private SessionFactory session;
 	}
 
 }
