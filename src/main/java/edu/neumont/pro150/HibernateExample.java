a3d0d9eaff62c7195a678eac02886e2fe2c5d06b
MShultz <mshultz@student.neumont.edu>
2016-11-14 17:14:53 -0700

    Added Edit to Example

--- src/main/java/edu/neumont/pro150/HibernateExample.java
+++ src/main/java/edu/neumont/pro150/HibernateExample.java
@@ -1,17 +1,14 @@
 package edu.neumont.pro150;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+
 import javax.persistence.PersistenceUnit;
 
-import org.hibernate.SessionFactory;
+
 
 public class HibernateExample {
 	
 	@PersistenceUnit
 	protected void setUp() throws Exception {
-		private SessionFactory session;
 	}
 
 }
